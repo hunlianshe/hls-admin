4591e4ac11be24302a5033636c48db874a18be00
Lisa5 <1348894516@qq.com>
2019-09-25 23:35:52 +0800

    feat: qsqy

--- src/pages/hls/index/components/userDetail/userDetail.js
+++ src/pages/hls/index/components/userDetail/userDetail.js
@@ -40,48 +40,48 @@ class UserDetail extends Component {
 
   render() {
 
-    return <div class="userDetail">
+    return <div className="userDetail">
       <div className="sub-title">用户详情</div>
       <div>
         <div className="info-title">基本信息</div>
-        <div class="info-cell">
+        <div className="info-cell">
           <div>昵称：hahhh</div>
           <div>手机：18887786789</div>
         </div>
-        <div class="info-cell">
+        <div className="info-cell">
           <div>性别：hahhh</div>
           <div>生日：18887786789</div>
         </div>
-        <div class="info-cell">
+        <div className="info-cell">
           <div>身高：hahhh</div>
           <div>收入：18887786789</div>
         </div>
-        <div class="info-cell">
+        <div className="info-cell">
           <div>身高：hahhh</div>
           <div>收入：18887786789</div>
         </div>
-        <div class="info-cell">
+        <div className="info-cell">
           <div>工作地区：hahhh</div>
           <div>学历：18887786789</div>
         </div>
-        <div class="info-cell">
+        <div className="info-cell">
           <div>是否想要孩子：hahhh</div>
           <div>是否有孩子：18887786789</div>
         </div>
-        <div class="info-cell">
+        <div className="info-cell">
           <div>婚姻状况：hahhh</div>
           <div>职业：18887786789</div>
         </div>
-        <div class="info-cell">
+        <div className="info-cell">
           <div>是否买房：hahhh</div>
         </div>
         <div>
           <div className="info-title">择偶标准</div>
-          <div class="info-cell">
+          <div className="info-cell">
             <div>年龄：hahhh</div>
             <div>收入：18887786789</div>
           </div>
-          <div class="info-cell">
+          <div className="info-cell">
             <div>身高：hahhh</div>
           </div>
         </div>
