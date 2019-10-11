c65ce5cbc1727738d5074948a10c823ef456c561
Lisa5 <1348894516@qq.com>
2019-10-12 00:37:35 +0800

    feat: add likelist

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  // HOST: 'https://www.peajs.top/testhls',
+  // HOST: 'http://148.70.243.198:8080',
   HOST: 'http://localhost:8009',
   MAPHOST: 'http://47.100.125.118:8080',
   MapServer:'http://47.100.125.118:8080/mnt/MapTiles',
