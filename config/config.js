7053dc79106acf2d404a2c64b19847baade83a1d
Lisa5 <1348894516@qq.com>
2019-10-16 22:10:40 +0800

    host

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 
 module.exports = {
-  // HOST: 'http://148.70.243.198:8080',
-  HOST: 'http://localhost:8009',
+  HOST: 'http://148.70.243.198:8080',
+  // HOST: 'http://localhost:8009',
   MAPHOST: 'http://47.100.125.118:8080',
   MapServer:'http://47.100.125.118:8080/mnt/MapTiles',
   HOSTPipeline: 'http://10.137.5.210:9001',
